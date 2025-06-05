074d19ec3fff1bcb3a928fa08651d98bb1a47757
Saermed Rauf <saermed.rauf@rdf.nuernberg.de>
2025-06-05 12:48:54 +0200

    benötigte importe

---
+++ noten_export.py
@@ -0,0 +1,5 @@
+from tkinter import filedialog, messagebox
+from openpyxl import Workbook
+from openpyxl.styles import Font, Alignment, PatternFill, Border, Side
+from daten_laden import lade_noten
+
