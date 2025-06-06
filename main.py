84501e72bd943e799545b90acbb3841ddfe11144
Jule Dietrich <jule.dietrich@rdf.nuernberg.de>
2025-06-06 10:06:55 +0200

    Main hinzugefügt

---
+++ main.py
@@ -0,0 +1,11 @@
+# Funktion: Einstiegspunkt des Programms
+
+# Importiere die Startfunktion für die grafische Benutzeroberfläche aus der gui.py
+from gui import zeige_login
+
+# Hauptfunktion
+# Beschreibung:
+# Wenn dieses Skript direkt ausgeführt wird (nicht importiert), wird das Loginfenster geöffnet.
+
+if __name__ == "__main__":
+    zeige_login()  # Starte das Login-Fenster für die Lehrkraft
